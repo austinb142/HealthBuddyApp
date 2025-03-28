8d8340f0bbae16e6221e66eb2f01e821e4b899bb
austinb142 <austin.borst@marquett.edu>
2025-03-28 11:38:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/healthbuddyapp/MyAppNavigation.kt
@@ -0,0 +1,28 @@
+package com.example.healthbuddyapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.NavHost
+import com.example.healthbuddyapp.pages.HomePage
+import com.example.healthbuddyapp.pages.LoginPage
+import com.example.healthbuddyapp.pages.SignUpPage
+
+
+@Composable
+fun MyAppNavigation(modifier: Modifier = Modifier, authViewModel: AuthViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login", builder = {
+        composable("login") {
+            LoginPage(modifier, navController, authViewModel)
+        }
+        composable("signup") {
+            SignUpPage(modifier, navController, authViewModel)
+        }
+        composable("home") {
+            HomePage(modifier, navController, authViewModel)
+        }
+    })
+}
